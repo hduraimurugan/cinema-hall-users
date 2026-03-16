c87ce70046643e6ff3d1d8f4fe4667d1f2ea4b90
Duraimurugan-H <hduraimurugan@gmail.com>
2026-03-16 21:14:26 +0530

    refactor: add margin to main content area for improved layout spacing

--- src/components/CinemaLayout.jsx
+++ src/components/CinemaLayout.jsx
@@ -14,7 +14,7 @@ export function CinemaLayout() {
       {/* Main Content */}
       <main className="flex-1 overflow-y-auto min-h-0">
         <div className="min-h-full flex flex-col">
-          <div className="flex-1">
+          <div className="flex-1 mb-20">
             <Outlet />
           </div>
 
